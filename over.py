4eb8785d09e995a4a226f34773a810f65133355a
Gennadii <g.mishchevskii@gmail.com>
2015-07-09 09:40:49 +0300

    Changed path to dropbox directory using ~user

--- over.py
+++ over.py
@@ -1,14 +1,16 @@
 from datetime import datetime
+import os.path
 
 today = str(datetime.now().year) + '.' + str(datetime.now().month) + '.' + str(datetime.now().day)
 yesterday = str(datetime.now().year) + '.' + str(datetime.now().month) + '.' + str(datetime.now().day-1)
 someday = str(datetime.now().year) + '.' + str(datetime.now().month) + '.'
+direction = os.path.expanduser(r'~\Dropbox\Work\Python\Programms\txt\over.txt')
 total = 0
 
 time = input('time = ')
 
 if len(time) == 0: # Print the data and count current overtimes
-	over = open('over.txt','r')
+	over = open(direction,'r')
 	line = over.readline()
 	while line:
 		print(line)		
@@ -35,7 +37,8 @@ Instance of <time>: 1, 1.30, 1.55, 1,00
 	exit()
 
 elif '*' not in time and datetime.now().hour >= 19: # Adding overtime for today 
-	over = open('over.txt','a')
+	over = open(direction,'a')
+	# over = open('over.txt','a')
 	if len(time) == 1:
 		over.write(today + ' - ' + time + '.00' + '\n') # If only hour entered adding minutes
 	else:
@@ -44,7 +47,7 @@ elif '*' not in time and datetime.now().hour >= 19: # Adding overtime for today
 elif (time[0] == '*' and time[-1] != '*') or (datetime.now().hour < 19 and '*' not in time): # Adding overtime for yestarday
 	if datetime.now().hour < 19:
 		time = '*' + time
-	over = open('over.txt','a')
+	over = open(direction,'a')
 	if len(time) == 2:
 		over.write(yesterday + ' - ' + time[1:] + '.00' + '\n')
 	else:
@@ -53,14 +56,14 @@ elif (time[0] == '*' and time[-1] != '*') or (datetime.now().hour < 19 and '*' n
 elif time[0] == '*' and time[-1] == '*': # Adding time for any date this year
 	month_day = input('Date: ')
 	date = str(datetime.now().year) + '.' + month_day
-	over = open('over.txt','a')
+	over = open(direction,'a')
 	if len(time) == 3:
 		over.write(date + ' - ' + time[1:-1] + '.00' + '\n')
 	else:
 		over.write(date + ' - ' + time[1:-1] + '\n')
 
 elif time[0] != '*' and '*' in time: # Adding time for any day of current month
-	over = open('over.txt','a')
+	over = open(direction,'a')
 	if len(time[time.find('*'):]) < 3:
 		over.write(someday + time[:time.find('*')] + ' - ' + time[time.find('*')+1:] + '.00' + '\n')
 	else:
@@ -68,7 +71,7 @@ elif time[0] != '*' and '*' in time: # Adding time for any day of current month
 
 over.close()
 
-o = open('over.txt','r') # Adding all overtimes to list to show up the last one
+o = open(direction,'r') # Adding all overtimes to list to show up the last one
 line = o.readline()
 data = []
 while line:
